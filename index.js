4c434dd69deef2930a9488c42cde51050e6a5ef9
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2022-12-05 07:54:40 +0300

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 // Import stylesheets
 import './style.css';
 
+
+
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+/*
+function keyword creates function
+the name of the funcion is calculate bill
+the body of the function is the js written
+between the curly brackets {}
+*/
+function calculateBill(billAmount, taxRate) {
+  console.log('running the bill');
+  const total = billAmount * (1 + taxRate);
+  return total;
+} 
+
+//The syntax below calls(runs) the function
+
+const myTotal = calculateBill(100, 0.13);
+
+console.log(myTotal);
